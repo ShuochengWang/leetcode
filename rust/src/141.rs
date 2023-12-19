287c01ef7d5a400269f4251cade23c5ddcc92283
Shuocheng Wang <wangshch5@outlook.com>
2023-12-19 23:32:38 +0800

    update

---
+++ rust/src/141.rs
@@ -0,0 +1,30 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution {}
+
+impl Solution {
+    fn has_cycle(head: Option<Box<ListNode>>) -> bool {
+        let mut fast = &head;
+        let mut slow = &head;
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+            slow = &slow.as_ref().unwrap().next;
+            if fast == slow {
+                return true;
+            }
+        }
+        false
+    }
+}
